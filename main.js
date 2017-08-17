c6bb8066460e00a439a29018f14b93b6a7efd156
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2017-08-17 02:11:20 -0600

    got minimum requirements for homework assignment functioning

--- main.js
+++ main.js
@@ -10,6 +10,13 @@ $(document).ready(function() {
 		
 		this.nowPlaying = playing;
 		var played = $("#player").get(0);
+		var name = $("#songName").get(0);
+		var artist = $("#songArtist").get(0);
+		var album = $("#songAlbum").get(0);
+		var scTrackLink = $("#songLink").get(0);
+		var scUserLink = $("#artistLink").get(0);
+		var descrAndGenre = $("#descriptionGenre").get(0);
+		var artwork = $("#artwork").get(0);
 		this.playlist = [];
 		var songNum = 0;
 		this.SCtrack = [];
@@ -41,25 +48,25 @@ $(document).ready(function() {
 		this.loadNext = function() {
       		songNum++;
         	played.src = this.playlist[songNum].source;
-        	document.getElementById("songName").innerText = this.playlist[songNum].name;
-        	document.getElementById("songArtist").innerText = this.playlist[songNum].artist;
-        	document.getElementById("songAlbum").innerText = this.playlist[songNum].album;
+        	name.innerText = this.playlist[songNum].name;
+        	artist.innerText = this.playlist[songNum].artist;
+        	album.innerText = this.playlist[songNum].album;
     	};
 
     	this.loadPrev = function() {
      		songNum--;
         	played.src = this.playlist[songNum].source;
-        	document.getElementById("songName").innerText = this.playlist[songNum].name;
-        	document.getElementById("songArtist").innerText = this.playlist[songNum].artist;
-        	document.getElementById("songAlbum").innerText = this.playlist[songNum].album;
+        	name.innerText = this.playlist[songNum].name;
+        	artist.innerText = this.playlist[songNum].artist;
+        	album.innerText = this.playlist[songNum].album;
    		};
 		
 		this.loadRandom = function() {
 			songNum = Math.floor((Math.random() * 3) + 0);
 			played.src = this.playlist[songNum].source;
-        	document.getElementById("songName").innerText = this.playlist[songNum].name;
-        	document.getElementById("songArtist").innerText = this.playlist[songNum].artist;
-        	document.getElementById("songAlbum").innerText = this.playlist[songNum].album;
+        	name.innerText = this.playlist[songNum].name;
+        	artist.innerText = this.playlist[songNum].artist;
+        	album.innerText = this.playlist[songNum].album;
 		}
    		
 
@@ -68,19 +75,24 @@ $(document).ready(function() {
 		};	
 
 		this.addSCtrack = function() {
-	
-       		path = $("#trackUrl").val();
-			SC.resolve(path).then(function(tracks){
-     			trackid = "tracks/" + tracks.id;
-     			SC.get(trackid).then(function(tracks){
-					played.src = tracks.stream_url + "?client_id=fd4e76fc67798bfa742089ed619084a6";
-
-				});
-     		});
+       		path = $("#trackID").val();
+     		trackid = "tracks/" + path;
+     		SC.get(trackid).then(function(tracks){
+				played.src = tracks.stream_url + "?client_id=fd4e76fc67798bfa742089ed619084a6";
+				name.innerText = tracks.title;
+				artist.innerText = tracks.user.username;
+				scTrackLink.href = tracks.permalink_url;
+				scTrackLink.innerText = "Check out the track page";
+				scUserLink.href = tracks.user.permalink_url;
+				scUserLink.innerText = "Check out the artist's page";
+				descrAndGenre.innerText = "Description: " + tracks.description + " Genre: " + tracks.genre; 
+				artwork.src = tracks.artwork_url + "?client_id=fd4e76fc67798bfa742089ed619084a6";
+			});
 		};
-     	
 	};
-		
+		SC.resolve('https://soundcloud.com/tankandthebangas/boxes-and-squares').then(function(results){
+			console.log(results);
+		});
 	
 	juke = new Jukebox(true);
 
